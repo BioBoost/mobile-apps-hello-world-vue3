80e5000ada771efc267a80e5b0ee37e245de606b
Nico De Witte <nico.dewitte@outlook.com>
2022-09-20 15:47:29 +0200

    Challenge on todo

---
+++ challenge.md
@@ -0,0 +1,5 @@
+Opdracht tegen volgende week:
+
+* Lijst tonen van todo items
+* Wanneer je er op klikt verdwijnen deze
+* Nieuwe toevoegen via een input box met button
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
   import SingleFireButton from '@/components/SingleFireButton.vue'
+  import { ref } from 'vue'
+
+  const quotes = ref([
+    'Chuck Norris breathes air ... five times a day.',
+    'Chuck Norris does not sleep. He waits.',
+    'Chuck Norris can speak Braille.'
+  ])
+
+  setTimeout(() => {
+    quotes.value.push('Chuck Norris can slam revolving doors.')
+  }, 10*1000);
+
 </script>
 
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <SingleFireButton />
+    <ul>
+      <li v-for="(q,i) in quotes" :key="i">{{ i }} : {{ q }}</li>
+    </ul>
   </div>
 </template>
 
