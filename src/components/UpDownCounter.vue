a6f95b0b2de13ddcdbb834a05418a732f6578ba8
Nico De Witte <nico.dewitte@outlook.com>
2022-09-20 14:59:58 +0200

    Show 2way binding

--- src/components/UpDownCounter.vue
+++ src/components/UpDownCounter.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
   // Component logic / state
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
 
   // Reactive state var
   const counter = ref(0);
@@ -21,6 +21,16 @@
     showQuote.value = counter.value >= 10;
   }
 
+  // Computed methods
+  const showChuck = computed(() => {
+    return counter.value >= 10
+  })
+
+  const color = computed(() => {
+    return (counter.value < 10 ? 'red' : 'green')
+  })
+
+// style="{color: red}"
 </script>
 
 <template>
@@ -29,8 +39,28 @@
   <button v-on:click="increment">Counter UP</button>
   <button @click="decrement">Counter DOWN</button>
 
-  <p>This website was build by {{ author }}</p>
+  <p v-bind:style="{'color': color}">This website was build by {{ author }}</p>
+  <p :style="{'color': color}">This website was build by {{ author }}</p>
+
+  <p :class="color">This website was build by {{ author }}</p>
 
   <p v-if="showQuote">Chuck Norris can dribble a bowling ball.</p>
   <p v-show="showQuote">The main export of Chuck Norris is pain.</p>
+  <p v-if="showChuck">Chuck Norris counted to infinity... twice.</p>
+  <img v-if="showChuck" src="https://upload.wikimedia.org/wikipedia/commons/5/5c/Chuck_Norris.jpg" />
+
+  <input type="text" v-model="counter" />
 </template>
+
+<style scoped>
+.red {
+  color: red;
+  font-weight: bolder;
+}
+
+.green {
+  color: green;
+  font-weight: bold;
+}
+
+</style>
