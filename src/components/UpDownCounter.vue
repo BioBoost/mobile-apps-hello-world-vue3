96153c9783d969a291bfabc57b092c70ed881135
Nico De Witte <nico.dewitte@outlook.com>
2022-09-20 14:09:04 +0200

    Part 1 of introduction vue

---
+++ src/components/UpDownCounter.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+  // Component logic / state
+  import { ref } from 'vue';
+
+  // Reactive state var
+  const counter = ref(0);
+    // Alternative: const data = reactive({ counter: 0 })
+
+  const showQuote = ref(false);
+
+  // Normal variable
+  let author = 'Nico De Witte'
+
+  function increment() {
+    counter.value++;
+    showQuote.value = counter.value >= 10;
+  }
+
+  function decrement() {
+    counter.value--;
+    showQuote.value = counter.value >= 10;
+  }
+
+</script>
+
+<template>
+  <p>Our counter is currently at <span>{{ counter }}</span></p>
+
+  <button v-on:click="increment">Counter UP</button>
+  <button @click="decrement">Counter DOWN</button>
+
+  <p>This website was build by {{ author }}</p>
+
+  <p v-if="showQuote">Chuck Norris can dribble a bowling ball.</p>
+  <p v-show="showQuote">The main export of Chuck Norris is pain.</p>
+</template>
---
+++ src/views/CounterView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+  import UpDownCounter from '@/components/UpDownCounter.vue'
+
+</script>
+
+<template>
+  <h1>Counter View</h1>
+  <p>Feel free to smash some buttons</p>
+  <UpDownCounter />
+</template>
